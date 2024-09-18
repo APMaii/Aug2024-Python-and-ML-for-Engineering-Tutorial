fd63c00d7392f0ddb4c885cca98df9a5ef518a09
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-09-18 21:06:57 +0330

    Add files via upload

---
+++ LL2.py
@@ -0,0 +1,417 @@
+
+"""
+In The NaME OF GOD
+
+Created on Wed Aug  7 17:50:34 2024
+
+@author: Ali Pilehvar Meibody
+
+
+1-harchizi nemitoni bnvsi
+2-noghte virgol .. alaki nabas bzni
+3- fasele sal am
+4-case sensetive a A  print Print
+
+
+
+python-->yek zaban hast
+1-kalamat (vocab)
+2-grammar (dastoorat)
+
+
+
+
+"""
+
+
+# salam man in tike ro be in dalil gozaashtam
+
+
+
+'''
+salama bbinid
+in code baraye
+felan chiz hast
+'''
+
+
+print('Hello world!')
+
+
+
+print('salam azizanam')
+
+print(salam azizanam)
+
+
+
+
+
+#----VOCAB--(kalamat)------
+#narenji ---> tabe haye dakheli (built in function)
+#banafsh---> dastoorat
+#baghie chiz haro -->sefid neshon mide
+
+
+
+print #60
+for  #30-40
+salam
+
+
+
+#---------------
+#az baal b paeen python mikhone
+#age kalame ro beshnase (funcrtion, dastoor) ejrash mikone
+#age nabashe --> esme yek zarf dar nazar migire
+
+fenjoon = 50
+
+
+zarf= 32893717167176372167267
+
+chae=560
+
+
+temperature=481.34
+
+
+#esme zarf-------
+ali=20
+ali2=
+ali_mm
+_ali=20
+Ali=30
+ali=40
+
+#nemishe XXXX---------
+2ali=  #nemishe
+*ali=
+open=
+#-------
+
+#SyntaxError: invalid syntax
+
+
+#thrickkkk
+#vaghty bishtar az yek kalame baraye zarfet esm mikhay bezari
+#osolish ine ke in dota kar ro koni
+thetemperature #mishe nevesht ama sakhte 
+TheTemperature
+the_temperature
+
+open
+openn
+open2
+open_
+Open
+
+
+#----------------------
+
+#ZARF SAKHTAN ---> assign (meghdar dehi)
+#ch type data mitonim tooye zarf zakhire konim?
+
+#Numbers ( int, float)  complex(a + bj)
+
+sen=17
+vazn=65.5
+bordar=1j
+
+
+#harf #kalame #jomle 
+
+#name
+
+
+name=mohsen
+#NameError: name 'mohsen' is not defined
+
+#harchi mikhay bnvis ag sefid beshe esme zrfe
+'   '
+
+name= 'mohsen'
+#str
+
+
+name=' Ali Pilehvar Meibody'
+
+
+sartitr=' emrooz jalaseye dovome AI hast va omidvaram le ..d.adlkdjaslDNAKJDHSBHDKSAB '
+
+
+name='a'
+
+
+
+#frgh dare
+name=ali
+name='ali'
+
+
+
+
+#assign (meghdar dehi)-->zarf
+#ba zarf ha chikar konim -->amalgara
+#----------
+# =  mosavi
+# + jam
+# - menha
+# * zarb
+# / taghsim 
+# ** tavan
+
+
+#moghayese
+# == 
+#  > <
+is
+and
+or
+#amalgeraha shabihe ham
+
+
+a=20
+b=30
+c=a+b
+d=a*b
+e=a**2
+
+
+
+
+a=20
+b=30
+c=a*b
+print(c)
+
+a=' salam khoobi?'
+print(a)
+
+
+print('salam khoobi')
+
+
+a=70
+print(a)
+
+
+print(70)
+
+
+
+#----------------
+#-----------------------------------------
+#sakhtar - structure
+'''
+1- Vorooodi (input)
+
+
+2- mohaasebat( calculation)
+
+
+
+3- Khoroji (namayesh)
+
+
+
+'''
+
+
+#----Vorooodi-----
+#1.1.assign
+a=45
+b=35
+
+
+#1.2. data (excell ) fromat
+c=open('C:/desktop/apm/chemsitry.csv')
+
+
+#1.3.input (karbar)--> karordi -->quiz
+input
+
+adad=input('Salam yek adade tasadofi lotfan entekhab kon :')
+
+
+#1.4. others -->az site , google , import 
+
+
+
+#2.mohasebat --> + - (amalgar ha) , for , if 
+#ketabkhone ha , hoosh masnooee -- model
+#har adadi ke too gehesmate vorodi gereftim
+#inja miaym roosh mohasebat anjam midim 
+#az yek jame sade begir ta hoshe masnooee
+
+
+
+
+#3-khooroji
+#3.1. namayesh bede
+print()
+
+#3.2. plot (tasvir)
+
+#3.3. save kone 
+
+#3.4 others -->website upload,.....
+
+
+
+#mesal------------
+#----1-vorodi
+a=40
+b=30
+
+#-----2-mohasebat
+c=a+b
+
+#3-khoroji
+print(c)
+
+
+
+#------tabe haye
+#sade tarin tabe
+print()
+#open() , input()
+
+a=20
+
+#bebinam zarfe a che no dade ee daronesh hast
+#yek tabe ee vojod dare b esme type()
+
+
+
+#esme zarfi k mikham bbinm dakhelesh ch noee has ro bayad bnvism
+type(a)
+#Out[8]: int
+#int-->sahih
+#float -->ashari
+#complex-->pichide
+#str-->reshte
+
+
+#andazash chghdre
+name='Ali'
+len(name)
+#Out[10]: 3
+#length -->tool
+
+
+name='alipilehvarmeibody'
+len(name)
+#Out[12]: 18
+type(name) #Out[13]: str
+
+
+
+#--------Overall------
+'''
+barname nevisi-->hadafehsh ine ke-->shoma be machine dastoor bedi
+
+zabane mane ensan-->english
+zabane machine --> binary --> 0 , 1
+niaz b yek zabane ma beyn (intermediate) hast
+
+
+Python yek zabane barname nevisi hast ke in vasat gharar migirad
+
+manande tamame zabn ha (mesle itlay, france)-->
+1-kalamt (vocab)
+2-grammar
+
+
+shoma inja codeto minevisi az bal b paeen (mesle neveshtan
+                                           too telegram)
+ta zmaani k run ro nazani chizi ejra nemishe (pas mitoni
+                                              hezarta taghir bedi)
+
+zamani ke run ro zadi python shabihe yek ensan az bala be paeen
+az chap be rast mikhonatesh
+harja hashtag # va ''' ''' bbine b onvane comment az roosh mipare
+pas ma mirtonim harjaye code bekhaym comment bezarim , note begim
+ya harchi azina estefade konim
+
+
+zamani k har kalame ee benevisim
+
+age reserve shode bashe (behsnase)-->rangi mishe
+
+baghieye kole code --> 
+1- built in function --> ( print()  namayesh
+                          , open()    file open mikard, 
+                          input()  az karbar chizi migrf
+                          type()  mirft migof noe dade chie
+                          len()   andaze ro migoft)
+2-dastoorat (if , for , ...)
+
+
+baghie chiz haro-->zarf -->sefid (deafult)
+zarf yek esm dare va shoam mitoni dakhelesh
+1-numbers(int, float,complex)  2-string reshte (str)--> horof,kalae,jomle
+**havaset bashe bayad ' ' 
+
+yekseri amlagar darim mesle + - * / ....
+
+
+va yadetam abshe hamishe
+yek code sakhtar dare:
+    1-ghesmate voroodi
+    2-ghesmate mohasebat
+    3-ghesmate khorojii
+    
+    
+    
+
+'''
+
+
+
+
+'''
+QUIZ.........
+dota zarf besaazi
+yek zarf esmeton
+yek zarf reshtatoon (nano, AI)
+
+
+va ba estefade az print() 
+
+
+
+khoroji (namayesh)---> 
+salam man ... hastam va be .. alaghe daram
+
+
+**bayad ba estefadeye dota zarf benevisi
+
+
+
+
+ai.2024.pilehvar@gmail.com
+
+TITLE--> Q_L2_NAME_LNAME
+
+
+Q_L2_AKBAR_AKBARI
+
+Q_L3_AKBAR_AKBARI
+
+
+'''
+
+#in paeeni ghaalte
+print('salam man ali hastam be NANO alaghe daram')
+
+
+
+
+
+
+
+
+
+
+
